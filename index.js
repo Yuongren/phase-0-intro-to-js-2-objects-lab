48dc97211fcd3e0b48e457145c929f62ce41774a
gitYoungren <youngrengitonga@gmail.com>
2022-11-22 08:46:18 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Write your solution in this file!
+// Write your solution in th
+function name(params) {
+    for (const key in employee)
+    delete employee[key]
+    employee.name='Sam' 
+}
+const obj = {
+    key1: value1,
+    key2:value2,
+};
+const obj ={
+    key1:value1
+    key2: {
+        innerkey1:innervalue1,
+        innerkey:innervalue2
+    },
+};
